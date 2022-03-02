589f904b71f26463e3a4e87d1d251572d7a40d92
mary-dalrymple <mary.s.dalrymple@gmail.com>
2022-03-01 19:33:53 -0500

    adds expenses template

--- project/expenses/views.py
+++ project/expenses/views.py
@@ -3,5 +3,6 @@ from django.http import HttpResponse
 from expenses.models import Summary, Detail
 
 def index(request):
-    total = Summary.objects.count()
-    return HttpResponse(f"Hello, world. You're at the expenses index, and it has {total} records.")
+  total_summaries = Summary.objects.count()
+  total_detail = Detail.objects.count()
+  return HttpResponse(f"Hello, world. You're at the expenses index and there are {total_summaries} summary records and {total_detail} records in the database.")
